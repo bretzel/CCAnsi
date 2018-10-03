39fbd21735c2b8a249ea730017fd09012fd12d40
bretzelus <bretzelus@outlook.com>
2018-10-03 11:36:42 -0400

    Restored post-build script;

--- Test/main.cc
+++ Test/main.cc
@@ -9,6 +9,7 @@ auto main() -> int{
     Ansi::CAnsi Con;
     Con.Init();
     Con << AnsiColor::Cyan << "Salutations" << AnsiColor::HGreen << "!!!!\n" << AnsiColor::Endl;
+    Con << AnsiColor::Orange << " Orange!" << AnsiColor::Endl;
 
     return 0;
 }
